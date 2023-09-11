3e1da78c263940a6194540cdcf548028c05c3911
Jhullya2720 <108037255+Jhullya2720@users.noreply.github.com>
2023-09-10 21:47:51 -0300

    Tarefa finalizada

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 function isPalindrome(str){
-    /* Seu código aqui */
-}
+        str = str.replace(/\s/g, '').toLowerCase();
+      
+        let start = 0;
+        let end = str.length - 1;
+    
+        for (let i = 0; i < str.length; i++) {
+          if (str[start] !== str[end]) {
+            return false;
+          }
+          start++;
+          end--;
+        }
+      
+        return true;
+      }
+
 
 function arrayMaxMin(arr){
     /* Seu código aqui */
 }
+isPalindrome()
