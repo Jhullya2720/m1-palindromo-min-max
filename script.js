8cd63ba87288f8825fe48164dbb9e536ca0bb1a6
Jhullya2720 <108037255+Jhullya2720@users.noreply.github.com>
2023-09-11 22:23:48 -0300

    Entrega finalizada

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-function isPalindrome(str){
-        str = str.replace(/\s/g, '').toLowerCase();
-      
-        let start = 0;
-        let end = str.length - 1;
-    
-        for (let i = 0; i < str.length; i++) {
-          if (str[start] !== str[end]) {
-            return false;
-          }
-          start++;
-          end--;
-        }
-      
-        return true;
-      }
+function isPalindrome(str) {
+  str = str.replace(/\s/g, '').toLowerCase();
 
+  let start = 0;
+  let end = str.length - 1;
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+  for (let i = 0; i < str.length; i++) {
+    if (str[start] !== str[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+
+  return true;
+}
+
+function arrayMaxMin(arr) {
+  if (arr.length == 0) {
+    return [];
+  }
+  let min = arr[0]
+  let max = arr[0]
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  let resultado = [min, max];
+  return resultado
 }
 isPalindrome()
+arrayMaxMin()
